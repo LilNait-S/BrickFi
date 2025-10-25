307f594013839f647039e7bc098f222819024922
Nait-s <lilnait.santos@gmail.com>
2025-10-25 18:47:07 -0500

    refactor: restructure Providers component to include SessionProvider and RainbowKitSiweNextAuthProvider

--- providers/index.tsx
+++ providers/index.tsx
@@ -2,10 +2,15 @@
 
 import "@rainbow-me/rainbowkit/styles.css"
 import { config, sonicTestnet } from "@/wagmi-config"
-import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import {
+  darkTheme,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit"
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 import { WagmiProvider } from "wagmi"
 import { Toaster } from "@/components/ui/sonner"
+import { SessionProvider } from "next-auth/react"
+import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth"
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,20 +25,24 @@ const queryClient = new QueryClient({
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-          theme={darkTheme({
-            accentColor: "oklch(0.6216 0.1348 161.9518)",
-            accentColorForeground: "oklch(0.985 0 0)",
-            borderRadius: "medium",
-          })}
-          modalSize="wide"
-          initialChain={sonicTestnet.id}
-        >
-          {children}
-          <Toaster richColors theme="light" closeButton />
-        </RainbowKitProvider>
-      </QueryClientProvider>
+      <SessionProvider refetchInterval={0}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitSiweNextAuthProvider>
+            <RainbowKitProvider
+              theme={darkTheme({
+                accentColor: "oklch(0.6216 0.1348 161.9518)",
+                accentColorForeground: "oklch(0.985 0 0)",
+                borderRadius: "medium",
+              })}
+              modalSize="wide"
+              initialChain={sonicTestnet.id}
+            >
+              {children}
+              <Toaster richColors theme="light" closeButton />
+            </RainbowKitProvider>
+          </RainbowKitSiweNextAuthProvider>
+        </QueryClientProvider>
+      </SessionProvider>
     </WagmiProvider>
   )
 }
