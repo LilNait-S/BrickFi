701b278705e1ad591840300aee12b935655c7a14
Nait-s <lilnait.santos@gmail.com>
2025-10-26 08:58:52 -0500

    fix: rename PoolConfig to poolConfig for consistency and update formatting

--- services/pool/config.ts
+++ services/pool/config.ts
@@ -4,8 +4,13 @@ import { sonicTestnet } from "@/wagmi-config"
 import { Address } from "viem"
 import { POOL_ABI } from "./abi"
 
-export const PoolConfig = {
+export const poolConfig = {
   address: "0x0748cA7E2Cb2Ba6f02AbBc24f903b61268CF095C" as Address,
   abi: POOL_ABI,
   chainId: sonicTestnet.id,
 } as const
+
+export const poolSummaryConfig = {
+  abi: POOL_ABI,
+  chainId: sonicTestnet.id,
+}
